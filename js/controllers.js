2ba535e4e6a5ad039b208840740b8823c5d821cc
David Bayless <David.C.Bayless15@gmail.com>
2016-01-29 08:54:27 -0700

    made service return a resolved promise to chain off of in controller

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,12 @@
-app.controller('pokeController', [pokeController]);
+app.controller('pokeController', ['$http', '$scope', 'pokeService', pokeController]);
 // Change Me
 
-function pokeController() {
+function pokeController($http, $scope, pokeService) {
   var vm = this;
   vm.title = 'Angular Template';
+  vm.pokemons = [];
+  pokeService.then(function(pokemon) {
+    vm.pokemons = pokemon;
+  });
+
 }
--- js/services.js
+++ js/services.js
@@ -1,5 +1,28 @@
-app.service('pokeService', ['$http', pokeService]);
+app.service('pokeService', ['$http', '$q', pokeService]);
 
-function pokeService($http) {
-  
-}
+function pokeService($http, $q) {
+    var pokemons = [];
+    function randPokemon() {
+      return Math.floor(Math.random() * 778);
+    }
+
+    // function randNum() {
+    //   return Math.floor(Math.random(1) * 151).toString();
+    // }
+    // var num = randNum();
+    // var url = "http://pokeapi.co/api/v1/pokedex/1/";
+    return $q(function(resolve, reject) {
+      $http.get("http://pokeapi.co/api/v1/pokedex/1/")
+      .then(function(pokemon) {
+        console.log(pokemon);
+        pokemons.push(pokemon.data.pokemon[0]);
+        pokemons.push(pokemon.data.pokemon[1]);
+        pokemons.push(pokemon.data.pokemon[2]);
+        pokemons.push(pokemon.data.pokemon[3]);
+        pokemons.push(pokemon.data.pokemon[4]);
+        resolve(pokemons);
+      }).catch(function(err) {
+        console.log(err);
+      });
+    });
+  }
